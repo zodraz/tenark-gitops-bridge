c17d56613d5ce3c6bf59f5eaccc5535bbc2c61da
zodraz <zodraz@fake.com>
2025-06-03 07:09:06 +0200

    Reintroduce providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -47,15 +47,15 @@ provider "azurerm" {
 #   filename = "${path.module}/kubeconfig"
 # }
 
-# provider "kubernetes" {
-#   config_path = local_file.kubeconfig.filename
+provider "kubernetes" {
+  # config_path = local_file.kubeconfig.filename
 
-# }
+}
 
-# provider "helm" {
-#   kubernetes {
-#     config_path = local_file.kubeconfig.filename
-#   }
+provider "helm" {
+  kubernetes {
+    # config_path = local_file.kubeconfig.filename
+  }
 
-# }
+}
 provider "random" {}
