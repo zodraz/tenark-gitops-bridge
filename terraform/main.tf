fbd27d08f9d23dba972ccc0597b171ff51a60999
zodraz <zodraz@fake.com>
2025-06-03 19:37:58 +0200

    fix acr name

--- terraform/main.tf
+++ terraform/main.tf
@@ -267,7 +267,7 @@ module "gitops_bridge_bootstrap" {
 ################################################################################
 
 resource "azurerm_container_registry" "acr" {
-  name                = "tenark-registry"
+  name                = "acrtenark"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   sku                 = "Standard"
