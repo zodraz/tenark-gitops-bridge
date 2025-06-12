6843450de136b5f13ef6b2e99c4f4a94596ba78d
zodraz <zodraz@fake.com>
2025-06-12 15:32:32 +0200

    Fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -298,13 +298,6 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
 }
 
-output "container_registry_name" {
-  value = azurerm_container_registry.acr.name
-}
-
-output "container_registry_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
 
 ################################################################################
 # KeyVault
