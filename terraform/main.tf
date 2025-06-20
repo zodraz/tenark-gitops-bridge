d8757b830a1dc0fcad360be18fede69f12dd0091
zodraz <zodraz@fake.com>
2025-06-20 11:15:39 +0200

    Lots of changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -420,3 +420,34 @@ resource "azurerm_dns_zone" "zone" {
   )
   resource_group_name = azurerm_resource_group.this.name
 }
+
+################################################################################
+#  Sealed Secrets
+################################################################################
+
+resource "kubernetes_namespace" "sealed-secrets-ns" {
+  metadata {
+    name = "sealed-secrets"
+  }
+}
+
+resource "kubernetes_secret" "sealed-secrets-key" {
+  depends_on = [kubernetes_namespace.sealed-secrets-ns]
+  metadata {
+    name      = "sealed-secrets-key"
+    namespace = "sealed-secrets"
+  }
+  data = {
+    "tls.crt" = file("keys/tls.crt")
+    "tls.key" = file("keys/tls.key")
+  }
+  type = "kubernetes.io/tls"
+}
+
+resource "helm_release" "sealed-secrets" {
+  depends_on = [kubernetes_secret.sealed-secrets-key]
+  chart      = "sealed-secrets"
+  name       = "sealed-secrets"
+  namespace  = "sealed-secrets"
+  repository = "https://bitnami-labs.github.io/sealed-secrets"
+}
